a81d80ab79d3da23411c50070974c822e60852ae
neumond <knifeslaughter@gmail.com>
2014-03-30 03:47:01 +0400

    new mapselect

--- gtkradiant/install/pkg/mapselect.sh
+++ gtkradiant/install/pkg/mapselect.sh
@@ -1,55 +1,70 @@
 #!/bin/bash
-
+#
 # usage:
 #
 # ./mapselect.sh mapname
 #
 # selects
-# map-<mapname>_<ver>.pk3dir
-# <ver> is a number
+# map-<mapname>_source.pk3dir
+#
+# you should copy your source dir when you finish its version
+# cp -R map-mymap_source.pk3dir map-mymap_1.pk3dir
+#
+# to clean up all symlinks:
+#
+# ./mapselect.sh
+
+
+# when you create new dirs or files in your map dir
+# and wany symlinks to be created for them
+# add them here:
+DIRS='env maps music textures sound'
+FILES='scripts/${M}.shader scripts/q3map2_${M}.shader scripts/${M}.arena'
+
 
 set +e
-
-cd "$( dirname "${BASH_SOURCE[0]}" )"
-
 MAP="$1"
+REQUIRED_DIRS="maps scripts textures"
+REQUIRED_FILES="maps/${MAP}.map scripts/${MAP}.shader scripts/q3map2_${MAP}.shader scripts/${MAP}.arena"
 
-for D in `echo map-${MAP}_*.pk3dir` ; do
-    if [ -d $D ]; then
-        IX=$(echo "$D" | grep -oE '_([0-9]+)\.' | tr -d '_.')
-        VERSIONS[$IX]=$D
-    fi
+# cleanup symlinks
+
+M='*'
+FLIST=$(eval "echo $FILES")
+echo "Removing symlinks.."
+for F in $FLIST ; do
+  if [ -L "$F" ]; then { rm "$F" ; echo "  $F" ; } ; fi
 done
-
-if (( ${#VERSIONS[@]} == 0 )); then
-    LASTVER="map-${MAP}_1.pk3dir"
-    mkdir "$LASTVER"
-else
-    LASTVER=${VERSIONS[-1]}
-    if (( ${#VERSIONS[@]} > 1 )); then
-        echo "You have many versions of this map, taking last ($LASTVER)"
-    fi
-fi
-
-DIRS="maps music textures"
-
-if [ ! -d "$LASTVER/maps" ]; then
-    mkdir "$LASTVER/maps"
-fi
-
 for D in $DIRS ; do
-    if [ -L $D ]; then
-        rm $D
-    fi
-
-    if [ -e $D ]; then
-        echo "$D is not a symlink! Can't delete it, aborting"
-        exit
-    fi
-
-    if [ -d "$LASTVER/$D" ]; then
-        ln -s "$LASTVER/$D" $D
-    fi
+  if [ -L "$D" ]; then { rm "$D" ; echo "  $D" ; } fi
 done
 
-echo "$LASTVER selected."
+# exit if no map chosen
+
+if [ -z "$MAP" ]; then exit ; fi
+
+# autocreate dirs and files
+
+MAPDIR="map-${MAP}_source.pk3dir"
+
+echo "Autocreating.."
+for D in $REQUIRED_DIRS ; do
+  if [ ! -d "$MAPDIR/$D" ]; then { mkdir -p "$MAPDIR/$D" ; echo "  $D" ; } fi
+done
+for F in $REQUIRED_FILES ; do
+  if [ ! -f "$MAPDIR/$F" ]; then { touch "$MAPDIR/$F" ; echo "  $F" ; } fi
+done
+
+# symlinks
+
+M="$MAP"
+FLIST=$(eval "echo $FILES")
+echo "Making symlinks.."
+for D in $DIRS ; do
+  if [ -d "$MAPDIR/$D" ]; then { ln -s "$MAPDIR/$D" "$D" ; echo "  $D" ; } fi
+done
+for F in $FLIST ; do
+  if [ -f "$MAPDIR/$F" ]; then { ln -s "$(pwd)/$MAPDIR/$F" "$F" ; echo "  $F" ; } fi
+done
+
+echo "Done. Selected: $MAPDIR"
