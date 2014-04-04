a7583db99571ae08f5f32d059b031d2ea3336363
Viech <viech@unvanquished.net>
2014-04-04 12:35:48 +0200

    Add a switch to allow prepending of a header file. Also introduce short switches.

--- src/export-netradiant.sh
+++ src/export-netradiant.sh
@@ -6,4 +6,4 @@ ROOT="../netradiant/gamepack"
 
 mkdir -p "${ROOT}/unvanquished.game/pkg"
 install -m644 game.xlink "${ROOT}/unvanquished.game/"
-./entities/ent_yaml2new.py --generate --dummyflag entities/entities.yaml > "${ROOT}/unvanquished.game/pkg/entities.def"
+./entities/ent_yaml2new.py -g -d -p entities/header.txt entities/entities.yaml > "${ROOT}/unvanquished.game/pkg/entities.def"
