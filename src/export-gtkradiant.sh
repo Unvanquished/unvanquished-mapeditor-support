b00b4c3cc1417542064c509e6786e505991c68a0
neumond <knifeslaughter@gmail.com>
2014-05-15 07:30:32 +0400

    New entities.def generated

--- src/export-gtkradiant.sh
+++ src/export-gtkradiant.sh
@@ -5,4 +5,4 @@ cd $( dirname $0 )
 ROOT="../gtkradiant"
 
 install -m644 game.xlink "${ROOT}/game/"
-./entities/ent_yaml2new.py -g -d -p entities/header.txt entities/entities.yaml > "${ROOT}/install/pkg/scripts/entities.def"
+./entities/ent_yaml2new.py -gdTDRE -p entities/header.txt entities/entities.yaml > "${ROOT}/install/pkg/scripts/entities.def"
